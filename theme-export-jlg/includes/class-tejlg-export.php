28f42b93ca60ba5c30968290c540d36f3d2b3188
Jewoc_DEV <jerome.legousse@gmail.com>
2025-10-05 20:47:37 +0200

    Adjust schedule calculation and add tests

--- theme-export-jlg/includes/class-tejlg-export.php
+++ theme-export-jlg/includes/class-tejlg-export.php
@@ -184,17 +184,56 @@ class TEJLG_Export {
         $hour           = isset($run_time_parts[0]) ? (int) $run_time_parts[0] : 0;
         $minute         = isset($run_time_parts[1]) ? (int) $run_time_parts[1] : 0;
 
-        $next_run = $now->setTime($hour, $minute, 0);
+        $next_run  = $now->setTime($hour, $minute, 0);
+        $frequency = isset($normalized['frequency']) ? (string) $normalized['frequency'] : 'daily';
 
         if ($next_run->getTimestamp() < $reference_time) {
-            if ('weekly' === $normalized['frequency']) {
-                $next_run = $next_run->modify('+1 week');
-            } else {
-                $next_run = $next_run->modify('+1 day');
+            switch ($frequency) {
+                case 'hourly':
+                    $next_run = self::advance_to_next_interval($next_run, (int) $reference_time, HOUR_IN_SECONDS);
+
+                    break;
+
+                case 'twicedaily':
+                    $next_run = self::advance_to_next_interval($next_run, (int) $reference_time, 12 * HOUR_IN_SECONDS);
+
+                    break;
+
+                case 'weekly':
+                    $next_run = $next_run->modify('+1 week');
+
+                    break;
+
+                default:
+                    $next_run = $next_run->modify('+1 day');
             }
         }
 
         return (int) $next_run->getTimestamp();
+    }
+
+    private static function advance_to_next_interval(\DateTimeImmutable $start, $reference_time, $interval_in_seconds) {
+        if ($interval_in_seconds <= 0) {
+            return $start;
+        }
+
+        $diff = $reference_time - $start->getTimestamp();
+
+        if ($diff < 0) {
+            return $start;
+        }
+
+        $steps = (int) floor($diff / $interval_in_seconds);
+
+        if ($steps > 0) {
+            $start = $start->modify(sprintf('+%d seconds', $steps * $interval_in_seconds));
+        }
+
+        if ($start->getTimestamp() < $reference_time) {
+            $start = $start->modify(sprintf('+%d seconds', $interval_in_seconds));
+        }
+
+        return $start;
     }
 
     private static function get_site_timezone() {
